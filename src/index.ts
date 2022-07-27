131d35eac08eaf7834c3acd05c452002fd5390cf
pavpench <p.penchev90@gmail.com>
2022-07-27 14:20:32 +0200

    initial_commit

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,91 @@
-/**
- * This file is just a silly example to show everything working in the browser.
- * When you're ready to start on your site, clear the file. Happy hacking!
- **/
+import { v4 as uuidV4 } from "uuid";
+type Task = {
+  id: string;
+  title: string;
+  completed: boolean;
+  createdAt: Date;
+};
 
-import confetti from 'canvas-confetti';
+const list = document.querySelector<HTMLUListElement>("#list");
+const completedTasksList = document.querySelector<HTMLUListElement>(
+  "#completed-tasks-list"
+);
+const form = document.querySelector<HTMLFormElement>("#new-task-form");
+const input = document.querySelector<HTMLInputElement>("#new-task-title");
+const tasks: Task[] = loadTasks();
+const completedTasks: Task[] = loadCompletedTasks();
 
-confetti.create(document.getElementById('canvas') as HTMLCanvasElement, {
-  resize: true,
-  useWorker: true,
-})({ particleCount: 200, spread: 200 });
+tasks.forEach(addListItem);
+addCompletedListItem(completedTasks);
+
+form?.addEventListener("submit", (e) => {
+  e.preventDefault();
+
+  if (input?.value == "" || input?.value == null) return;
+
+  const newTask: Task = {
+    id: uuidV4(),
+    title: input.value,
+    completed: false,
+    createdAt: new Date(),
+  };
+
+  tasks.push(newTask);
+  saveTasks();
+
+  addListItem(newTask);
+  input.value = "";
+});
+
+function addListItem(task: Task) {
+  const item = document.createElement("li");
+  const label = document.createElement("label");
+  const checkbox = document.createElement("input");
+  checkbox.type = "checkbox";
+  checkbox.checked = task.completed;
+
+  checkbox.addEventListener("change", () => {
+    task.completed = checkbox.checked;
+    tasks.forEach((task) => {
+      if (task.completed === true) {
+        tasks.splice(tasks.indexOf(task), 1);
+        completedTasks.push(task);
+      }
+    });
+    saveTasks();
+  });
+
+  label.append(checkbox, task.title);
+  item.append(label);
+  list?.append(item);
+}
+
+function addCompletedListItem(completedTasks: Task[]) {
+  saveTasks();
+
+  completedTasks.map((task) => {
+    const item = document.createElement("li");
+    const label = document.createElement("label");
+    const checkbox = document.createElement("input");
+    checkbox.type = "checkbox";
+    label.append(checkbox, task.title);
+    item.append(label);
+    completedTasksList?.append(item);
+  });
+}
+
+function saveTasks() {
+  localStorage.setItem("TASKS", JSON.stringify(tasks));
+  localStorage.setItem("COMPLETED_TASKS", JSON.stringify(completedTasks));
+}
+
+function loadTasks(): Task[] {
+  const taskJSON = localStorage.getItem("TASKS");
+  if (taskJSON == null) return [];
+  return JSON.parse(taskJSON);
+}
+function loadCompletedTasks(): Task[] {
+  const completedTasksJSON = localStorage.getItem("COMPLETED_TASKS");
+  if (completedTasksJSON == null) return [];
+  return JSON.parse(completedTasksJSON);
+}
